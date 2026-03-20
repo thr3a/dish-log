ba6d50533237a1faea83e00e6c01fb2749483a37
thr3a <thr3a@example.com>
2026-03-21 00:39:31 +0900

    fix:

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,7 +3,7 @@ import * as admin from 'firebase-admin';
 const getCredential = (): admin.credential.Credential => {
   const json =
     process.env.NODE_ENV === 'production' ? process.env.FIREBASE_SECRET_JSON : process.env.FIREBASE_SECRET_DEV_JSON;
-  // if (!json) throw new Error('Firebase認証情報が設定されていません');
+  if (!json) throw new Error('Firebase認証情報が設定されていません');
   // JSON.parse は any を返すため、firebase-admin の cert() にそのまま渡せる
   return admin.credential.cert(JSON.parse(json));
 };
